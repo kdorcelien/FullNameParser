9cf325a176e04d1b986c304b244a7dc69cadcace
kdorcelien <kdorcelien.kd@gmail.com>
2025-09-29 11:39:04 -0400

    Final commit

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class FullNameParser {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello, please enter your full name, first middle last: ");
+        String fullname = input.nextLine();
+        // System.out.println(fullname.trim());
+
+        String[] splitName = fullname.trim().split("\\ ");
+        String firstName = splitName[0];
+        String lastName = splitName[1];
+
+        System.out.print(firstName + " " + lastName);
+
+
+
+
     }
 }
